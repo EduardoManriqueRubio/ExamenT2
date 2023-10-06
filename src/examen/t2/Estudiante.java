e9aaf19f35390064c2f7791b438130717e7393ee
Eduardo <Eduardo@WIN-7J7TDH5DR1T>
2023-10-06 14:00:54 -0500

    Examen T2

---
+++ src/examen/t2/Estudiante.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package examen.t2;
+
+/**
+ *
+ * @author Eduardo
+ */
+public class Estudiante {
+    private int id;
+    private String nombre;
+    
+     public Estudiante() {
+    }
+    
+     public void setId(int id) {
+        this.id = id;
+    }
+     
+     public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+     
+     public int getId() {
+        return id;
+    }
+     
+     public String getNombre() {
+        return nombre;
+    }
+}
